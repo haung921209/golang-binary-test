b2564272bce2642167727b589c37ce75a1c81051
hyungmin.oh <hyungmin.oh@nhn.com>
2023-11-07 14:30:00 +0900

    panic recovery logic added

--- cmd/root.go
+++ cmd/root.go
@@ -72,6 +72,11 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	defer func() {
+		r := recover()
+		fmt.Println(r)
+		fmt.Println("panic recovered")
+	}()
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
